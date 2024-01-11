6fb48abaa081ae0fdb47a1db448da0e3350c96d3
H-Asmaa <asmaehadar32@gmail.com>
2024-01-11 15:55:52 +0100

    Committing a function that uses variable annotation and concatenates two strings.

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""0x00-python_variable_annotations"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """A function that uses variable annotation, to concatenate
+    two strings."""
+    return str1 + str2
