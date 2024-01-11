5e30ffc7fc8ee595ff2f59944fe1d69862c78ca5
H-Asmaa <asmaehadar32@gmail.com>
2024-01-11 17:28:00 +0100

    Committing a function that the annotation had been added to.

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """0x00-python_variable_annotations"""
-from typing import Sequence, Any, Union, Type, Optional
+from typing import Sequence, Any, Union
+from types import NoneType
 
-
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, Type[Optional[None]]]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
     """A function that we must add the variable annotation to."""
     if lst:
         return lst[0]
