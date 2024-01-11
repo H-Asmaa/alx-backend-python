72f2b9e52a4c9a9bb345053c206842ec02ceab00
H-Asmaa <asmaehadar32@gmail.com>
2024-01-11 15:59:46 +0100

    Committing a function that returns the floor of a float number;

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""0x00-python_variable_annotations"""
+
+
+def floor(n: float) -> int:
+    """A function that uses variable annotation, to do the sum
+    of two floats."""
+    return int(n)
