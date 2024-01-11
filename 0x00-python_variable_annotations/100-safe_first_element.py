a8af1b3af54308c589c6d8b1a72ad1574c9f6114
H-Asmaa <asmaehadar32@gmail.com>
2024-01-11 17:56:44 +0100

    Committing another trial to fix requirement fail.

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """0x00-python_variable_annotations"""
 from typing import Sequence, Any, Union
-from types import NoneType
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """A function that we must add the variable annotation to."""
     if lst:
         return lst[0]
