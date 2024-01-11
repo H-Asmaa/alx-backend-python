0f83cf0fb2810a2d6b78d49d13985a03212dd6b7
H-Asmaa <asmaehadar32@gmail.com>
2024-01-11 16:03:21 +0100

    Committing a function that returns the string representation of a float number.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""0x00-python_variable_annotations"""
+
+
+def to_str(n: float) -> str:
+    """A function that uses variable annotation, and returns the
+    string representation of a float."""
+    return str(n)
