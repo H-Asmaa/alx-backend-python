fba97335f1848508aa0b5fad2e1f1113f44e6749
H-Asmaa <asmaehadar32@gmail.com>
2024-01-11 16:55:39 +0100

    Committing a function that needed annotations. Adding missing Sequence.

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,6 +3,6 @@
 from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: Iterable) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """A function that we must add the variable annotation to."""
     return [(i, len(i)) for i in lst]
