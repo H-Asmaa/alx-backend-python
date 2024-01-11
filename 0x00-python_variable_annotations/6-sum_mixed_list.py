403a71db8fd95d641501ed1aecf6481dc751e7b9
H-Asmaa <asmaehadar32@gmail.com>
2024-01-11 16:22:41 +0100

    Committing a function that sums up a list of mixed elements, float and int.

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """A function that uses variable annotation, takes in a
     list of floats and ints and returns the float sum of elements.
     It uses Union to declaire mixed elements."""
