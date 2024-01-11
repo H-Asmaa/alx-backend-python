183f8cc5e39395ea25e60e922c87f8b87224fb67
H-Asmaa <asmaehadar32@gmail.com>
2024-01-11 16:38:26 +0100

    Committing a function that returns a tuple of mixed elements, there was no need for a Union in the returned value.

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import List, Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[Union[str, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """A function that uses variable annotation, takes a string k and a
     v being float or an int, and returns a tuple of mixed elements."""
     return (k, v * v)
