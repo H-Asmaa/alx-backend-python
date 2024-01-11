11a919e85440613696594b5361f38b6ed8e3b4e7
H-Asmaa <asmaehadar32@gmail.com>
2024-01-11 17:32:28 +0100

    Committing a function that the annotation had been added to. Fixing requirement fail.

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -3,6 +3,7 @@
 from typing import Sequence, Any, Union
 from types import NoneType
 
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
     """A function that we must add the variable annotation to."""
     if lst:
