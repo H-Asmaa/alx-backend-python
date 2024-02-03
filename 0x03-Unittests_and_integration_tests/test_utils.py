d75fa864a4489c0c88a87765e8bd4744add03734
H-Asmaa <asmaehadar32@gmail.com>
2024-02-03 19:48:36 +0100

    Committing a new method to unit test the keyErrors.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,6 +21,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """A method that tests access_nested_map util function."""
         self.assertEqual(access_nested_map(nested_map, path), expectedRes)
 
+    @parameterized.expand(
+        [
+            ({}, ("a",), KeyError, "'a'"),
+            ({"a": 1}, ("a", "b"), KeyError, "'b'")
+        ]
+    )
+    def test_access_nested_map_exception(
+        self, nested_map, path, expectedException, expectedExceptionMsg
+    ):
+        """A method that tests the keyError in access_nested_map
+        util function"""
+        with self.assertRaises(expectedException) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(e.exception), expectedExceptionMsg)
+
 
 if __name__ == "__main__":
     unittest.main()
