25c6b4c1809959a5109b2c9f26464730bb2cec19
H-Asmaa <asmaehadar32@gmail.com>
2024-03-29 20:23:51 +0000

    Committing an async routine that spawn wait_random n times with the specified max_delay.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,11 @@
 """
 0x01. Python - Async
 """
+from typing import List
 wait = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """an async routine that spawn wait_random n times with the
     specified max_delay."""
     delays = []
