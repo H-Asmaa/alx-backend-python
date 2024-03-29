533ba312515045de42391d0aa0bd1345a50fa1bb
H-Asmaa <asmaehadar32@gmail.com>
2024-03-29 19:01:55 +0000

    Committing an asynchronous coroutline that waits for a delay and returns a random float.

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+0x01. Python - Async
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """An asynchronous coroutine that waits for a random delay
+    between 0 and max_delay."""
+    await asyncio.sleep(max_delay)
+    return random.random()
