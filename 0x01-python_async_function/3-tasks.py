ae2a711236e01f4de8b6e578da271e87118b1c5e
H-Asmaa <asmaehadar32@gmail.com>
2024-03-30 01:58:07 +0000

    Committing a function returns an asyncio.Task pycodestyle correction

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,9 +2,10 @@
 """
 0x01. Python - Async
 """
-import time
 import asyncio
-wait_random = __import__('0-basic_async_syntax').wait_random
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """A function that returns an asyncio.Task."""
