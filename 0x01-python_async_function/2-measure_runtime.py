1749343eba1b5ce7174ea232f373049425bc9d78
H-Asmaa <asmaehadar32@gmail.com>
2024-03-30 02:20:14 +0000

    Committing A function measures the total execution time. Correction of output.

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -9,7 +9,7 @@ wait_n = __import__("1-concurrent_coroutines").wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
     """A function measures the total execution time."""
-    start = time.perf_counter()
+    start = time.time()
     asyncio.run(wait_n(n, max_delay))
-    end = time.perf_counter()
-    return end - start / n
+    total_time = time.time() - start
+    return total_time / n
